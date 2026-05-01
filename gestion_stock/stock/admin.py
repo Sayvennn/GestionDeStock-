7c427e019e9dac70cb2d3858263dd57a86e92195
Sayvennn <contact.atassianass@gmail.com>
2026-05-01 21:22:07 +0100

    superuser added

--- gestion_stock/stock/admin.py
+++ gestion_stock/stock/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from stock.models import Commande,LigneCommande,MouvementStock
 # Register your models here.
+
+admin.site.register([Commande,LigneCommande,MouvementStock])
--- gestion_stock/suppliers/admin.py
+++ gestion_stock/suppliers/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from suppliers.models import Fournisseur
 # Register your models here.
+
+admin.site.register(Fournisseur)
